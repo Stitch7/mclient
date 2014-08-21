43f952268a0e875f6ba49792695cb26810ffdadf
Christopher Reitz <cre@christohersmini.fritz.box>
2014-08-21 16:09:29 +0200

    Initial Commit

---
+++ mclient/MCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCLAppDelegate.h
+//  mclient
+//
+//  Created by Christopher Reitz on 21.08.14.
+//  Copyright (c) 2014 Christopher Reitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mclient/MCLDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  MCLDetailViewController.h
+//  mclient
+//
+//  Created by Christopher Reitz on 21.08.14.
+//  Copyright (c) 2014 Christopher Reitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCLDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
