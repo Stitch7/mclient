1b4eb145982dfc6aecf7f68b695883acf3237c13
Christopher Reitz <cre@macmini.fritz.box>
2014-09-29 15:16:46 +0200

    Readlist stores data now in userDefaults

--- mclient/MCLAppDelegate.m
+++ mclient/MCLAppDelegate.m
@@ -39,7 +39,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
--- mclient/MCLReadList.m
+++ mclient/MCLReadList.m
@@ -10,7 +10,8 @@
 
 @interface MCLReadList()
 
-@property (strong) NSMutableArray *messages;
+@property (strong, nonatomic) NSMutableArray *messages;
+@property (strong, nonatomic) NSUserDefaults *userDefaults;
 
 @end
 
@@ -19,7 +20,8 @@
 - (id)init
 {
     if (self = [super init]) {
-        self.messages = [[NSMutableArray alloc] initWithContentsOfFile:[self fileName]];
+        self.userDefaults = [NSUserDefaults standardUserDefaults];
+        self.messages = [[self.userDefaults objectForKey:@"readList"] mutableCopy];
         if (self.messages == nil) {
             self.messages = [[NSMutableArray alloc] init];
         }
@@ -30,22 +32,20 @@
 
 - (void)addMessageId:(NSNumber *)messageId
 {
-    [self.messages addObject:messageId];
-    [self.messages writeToFile:[self fileName] atomically:YES];
+    if ( ! [self messageIdIsRead:messageId]) {
+        [self.messages addObject:messageId];
+        [self.userDefaults setObject:self.messages forKey:@"readList"];
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [self.userDefaults synchronize];
+//            NSLog(@"%@ count: %lu", self.messages, (unsigned long)[self.messages count]);
+        });
+    }
 }
 
 - (BOOL)messageIdIsRead:(NSNumber *)messageId
 {
     return [self.messages containsObject:messageId];
-}
-
-- (NSString *)fileName
-{
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *fileName = [documentsDirectory stringByAppendingPathComponent:@"readlist.xml"];
-    
-    return fileName;
 }
 
 @end
