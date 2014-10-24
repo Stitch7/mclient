ace20f1b51e878d93e36e6b53688fad7e970911a
Christopher Reitz <cre@MacMini.fritz.box>
2014-10-24 02:16:59 +0200

    Removed forgotten UISplitViewControllerDelegate implementation

--- mclient/MCLMessageListWidmannStyleViewController.m
+++ mclient/MCLMessageListWidmannStyleViewController.m
@@ -614,31 +614,6 @@
 }
 
 
-#pragma mark - UISplitViewControllerDelegate
-
-
-/**
- *
- * IS THIS NEEDED ?????
- *
- */
-
-
-- (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
-{
-    barButtonItem.title = @"Threads";
-    [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
-    self.masterPopoverController = popoverController;
-}
-
-- (void)splitViewController:(UISplitViewController *)splitController willShowViewController:(UIViewController *)viewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
-{
-    // Called when the view is shown again in the split view, invalidating the button and popover controller.
-    [self.navigationItem setLeftBarButtonItem:nil animated:YES];
-    self.masterPopoverController = nil;
-}
-
-
 #pragma mark - MCLComposeMessageViewControllerDelegate
 
 - (void)composeMessageViewControllerDidFinish:(MCLComposeMessageViewController *)inController withType:(NSUInteger)type
