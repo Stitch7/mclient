547f9ae585f66fd5a7950f69b1339815b42551d4
Christopher Reitz <cre@MacMini.fritz.box>
2014-11-03 16:43:40 +0100

    Fix in showLoginStatus for missing loginData

--- mclient/MCLBoardListTableViewController.m
+++ mclient/MCLBoardListTableViewController.m
@@ -116,6 +116,7 @@
 
     if (username.length == 0 || password.length == 0) {
         [self saveValidLoginFlagToUserDefaultWithValue:NO];
+        [navToolbarView loginStatusNoLogin];
     } else {
         // Check login data async
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
