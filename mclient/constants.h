d53fa2187cb7f1f520dcbbc37276e819dae41eb1
Christopher Reitz <cre@MacMini.fritz.box>
2014-11-09 19:30:11 +0100

    Removed ifdef hullabaloo for kMServiceBaseURL

--- mclient/constants.h
+++ mclient/constants.h
@@ -6,12 +6,8 @@
 //  Copyright (c) 2014 Christopher Reitz. All rights reserved.
 //
 
-#ifdef DEBUG
-//    #define kMServiceBaseURL @"https://reitz.re:8080/mservice"
-    #define kMServiceBaseURL @"http://192.168.178.33:8080/mservice"
-#else
-    #define kMServiceBaseURL @"http://reitz.re:8080/mservice"
-#endif
+//#define kMServiceBaseURL @"http://192.168.178.33:8080/mservice"
+#define kMServiceBaseURL @"https://reitz.re:8080/mservice"
 
 #define kManiacForumURL @"http://www.maniac-forum.de/forum/pxmboard.php"
 
