aeeb6ec19c5a8a60f3d4e93d7f2a20137187e557
Christopher Reitz <christopher@reitz.re>
2016-12-23 22:22:08 +0100

    From now on use the nerds backend

--- mclient/constants.h
+++ mclient/constants.h
@@ -6,9 +6,8 @@
 //  Copyright (c) 2014 Christopher Reitz. All rights reserved.
 //
 
-//#define kMServiceBaseURL @"http://192.168.178.33:8080/mservice"
-#define kMServiceBaseURL @"https://reitz.re:8080/mservice"
-
+//#define kMServiceBaseURL @"https://reitz.re:8080/mservice"
+#define kMServiceBaseURL @"https://nerds.berlin/mservice/"
 #define kManiacForumURL @"http://www.maniac-forum.de/forum/pxmboard.php"
 
 #define kSettingsSignatureTextDefault NSLocalizedString(@"sent from M!client for iOS", nil)
