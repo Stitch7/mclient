69e42c3d9d3459bbf25eceba8f787fe492ef53ee
Christopher Reitz <christopher@reitz.re>
2017-03-24 21:32:56 +0100

    Housekeeping

--- mclient/constants.h
+++ mclient/constants.h
@@ -6,15 +6,12 @@
 //  Copyright (c) 2014 Christopher Reitz. All rights reserved.
 //
 
-//#define kMServiceBaseURL @"https://reitz.re:8080/mservice"
-//#define kMServiceBaseURL @"https://nerds.berlin/mservice/"
-//#define kMServiceBaseURL @"http://192.168.178.36:8080/"
-#define kMServiceBaseURL @"https://nerds.berlin/mservice-1.2/"
-
+//#define kMServiceBaseURL @"http://192.168.178.35:8080/"
+#define kMServiceBaseURL @"https://nerds.berlin/mservice/"
 
 #define kManiacForumURL @"http://www.maniac-forum.de/forum/pxmboard.php"
 
-#define kSettingsSignatureTextDefault NSLocalizedString(@"sent from M!client for iOS", nil)
+#define kSettingsSignatureTextDefault @"gesendet mit M!client für iOS"
 
 typedef NS_ENUM(NSUInteger, kMCLSettingsThreadView) {
     kMCLSettingsThreadViewWidmann,
