0427a345267c27fedd98a4302f09477d1100bacc
Christopher Reitz <christopher@reitz.re>
2017-04-05 19:11:03 +0200

    Code style

--- mclient/Views/MCLDetailView.m
+++ mclient/Views/MCLDetailView.m
@@ -32,41 +32,41 @@
 
 - (UIImageView *)image
 {
-	if (!_image) {
+    if (!_image) {
         _image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logoDetailView.png"]];
     }
 
-	return _image;
+    return _image;
 }
 
 - (UILabel *)subLabel
 {
-	if (!_subLabel) {
-		_subLabel = [[UILabel alloc] initWithFrame:self.bounds];
-		_subLabel.font = [UIFont systemFontOfSize:SUB_LABEL_SIZE];
-	}
-    
-	return _subLabel;
+    if (!_subLabel) {
+        _subLabel = [[UILabel alloc] initWithFrame:self.bounds];
+        _subLabel.font = [UIFont systemFontOfSize:SUB_LABEL_SIZE];
+    }
+
+    return _subLabel;
 }
 
 #pragma mark - Initializers
 
 - (id)initWithFrame:(CGRect)frame
 {
-	if (self = [super initWithFrame:frame]) {
+    if (self = [super initWithFrame:frame]) {
         [self configureWithSubLabelText:NSLocalizedString(@"Select a thread…", nil)];
-	}
+    }
 
-	return self;
+    return self;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
-	if (self = [super initWithCoder:aDecoder]) {
+    if (self = [super initWithCoder:aDecoder]) {
         [self configureWithSubLabelText:NSLocalizedString(@"Select a board…", nil)];
-	}
-    
-	return self;
+    }
+
+    return self;
 }
 
 - (void) dealloc
@@ -80,7 +80,7 @@
                                              selector:@selector(themeChanged:)
                                                  name:MCLThemeChangedNotification
                                                object:nil];
-    
+
     self.subLabel.text = subLabelText;
 
     [self addSubview:self.image];
@@ -102,22 +102,22 @@
                                                         attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:SUB_LABEL_SIZE]}
                                                            context:nil].size;
 
-	CGRect subLabelFrame;
-	subLabelFrame.size = subLabelSize;
-	self.subLabel.frame = subLabelFrame;
+    CGRect subLabelFrame;
+    subLabelFrame.size = subLabelSize;
+    self.subLabel.frame = subLabelFrame;
 
-	// Align label and spinner horizontaly
-	CGRect imageFrame = self.image.frame;
+    // Align label and spinner horizontaly
+    CGRect imageFrame = self.image.frame;
 
-	imageFrame.origin.x = self.bounds.origin.x + (self.bounds.size.width - imageFrame.size.width) / 2;
+    imageFrame.origin.x = self.bounds.origin.x + (self.bounds.size.width - imageFrame.size.width) / 2;
     subLabelFrame.origin.x = self.bounds.origin.x + (self.bounds.size.width - subLabelFrame.size.width) / 2;
 
-	// Set y position
+    // Set y position
     imageFrame.origin.y = (self.bounds.size.height - imageFrame.size.height) / 2;
     subLabelFrame.origin.y = imageFrame.origin.y + imageFrame.size.height + 20;
 
     // Pass back modiefied frames
-	self.image.frame = imageFrame;
+    self.image.frame = imageFrame;
     self.subLabel.frame = subLabelFrame;
 }
 
