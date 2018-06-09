8395f3cc527efb7a9253be21e768ae0c2bf0a64b
Christopher Reitz <christopher@reitz.re>
2018-06-09 11:49:09 +0200

    Disable zooming in webviews in iOS11

--- mclient/Models/MCLMessage.m
+++ mclient/Models/MCLMessage.m
@@ -153,7 +153,7 @@
     return [NSString stringWithFormat:@""
             "<html>"
             "<head>"
-            "<meta name=\"viewport\" content=\"initial-scale=1.0\"/>"
+            "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\"/>"
             "<script type=\"text/javascript\">"
             "    function spoiler(obj) {"
             "        if (obj.nextSibling.style.display === 'none') {"
